8448d329245442a3275ee2c15f532ec2c1227ab1
kelly6414143 <kelly6414143@gmail.com>
2020-07-21 09:26:16 +0800

    add router架構

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+// import routeConfig from './routeConfig'
 import './style/app.scss';
 import Homepage from './components/homepage/Homepage';
+import Roompage from './components/roompage/Roompage';
 
-const App = ()=> {
+const App = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <Router className="App" >
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/roomInfo/:type" component={Roompage} />
+        <Redirect from="/roomInfo/" to="/roomInfo/singleroom" />
+      </Switch>
+    </Router>
   );
 }
 
