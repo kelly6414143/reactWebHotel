b36bd8653c7afccdbc320f4b67e0465685e684d1
kelly6414143 <kelly6414143@gmail.com>
2020-07-21 15:22:53 +0800

    接收container傳來的rooms 顯示在view

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import HomepageConainer from '../../containers/homepage/Homepage'
+import '../../style/homepage.scss'
 
 const Homepage = (props) => {
-    // const {test} = props
+    const { rooms } = props
+    console.log(rooms)
     return (
-        <div>Homepage</div>
+        <div className="homepage">
+            <h1>WHITE INN</h1>
+            <div className="homepage_footer">
+                <div className="homepage_roomcarousel">
+                    {
+                        rooms.map((el) =>
+                            <div key={el.id} className="homepage_roomcarousel_roomcard">
+                                <div className="homepage_roomcarousel_imgwrapper" style={{ backgroundImage: `url(${el.imageUrl})` }}></div>
+                                <div className="homepage_roomcarousel_roominfo">
+                                    <h3>{el.name}</h3>
+                                    <p>$ {el.normalDayPrice}</p>
+                                    <p>$ {el.holidayPrice}</p>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
