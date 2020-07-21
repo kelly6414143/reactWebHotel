7686633c71a55648708a19bd41f125417c8d7fc5
kelly6414143 <kelly6414143@gmail.com>
2020-07-21 21:17:05 +0800

    新增footer資訊欄 roomcard改用link套

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HomepageConainer from '../../containers/homepage/Homepage'
 import '../../style/homepage.scss'
 
@@ -11,17 +12,25 @@ const Homepage = (props) => {
             <div className="homepage_footer">
                 <div className="homepage_roomcarousel">
                     {
-                        rooms.map((el) =>
-                            <div key={el.id} className="homepage_roomcarousel_roomcard">
+                        rooms.map((el) => {
+                            const trimRoomName = el.name.split(" ").join("")
+                            return (<Link to={`/roomInfo/${trimRoomName}`} key={el.id} className="homepage_roomcarousel_roomcard">
                                 <div className="homepage_roomcarousel_imgwrapper" style={{ backgroundImage: `url(${el.imageUrl})` }}></div>
                                 <div className="homepage_roomcarousel_roominfo">
                                     <h3>{el.name}</h3>
                                     <p>$ {el.normalDayPrice}</p>
                                     <p>$ {el.holidayPrice}</p>
                                 </div>
-                            </div>
+                            </Link>
+                            )
+                        }
                         )
                     }
+                </div>
+                <div className="homepage_webinfo">
+                    <p>台北市ＯＯ區ＯＯ街123號</p>
+                    <p>12345-6789</p>
+                    <p>abcd@efghijk.com</p>
                 </div>
             </div>
         </div>
