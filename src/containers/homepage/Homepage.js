c9c90e36a6b5c3b5a1e591eb945c92de71f23fe5
kelly6414143 <kelly6414143@gmail.com>
2020-07-21 15:22:13 +0800

    引入getRooms api 並傳到component

--- src/containers/homepage/Homepage.js
+++ src/containers/homepage/Homepage.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import api from '../../apis/api'
 
-const HomepageConainer= (WrappedComponent)=> class extends React.Component {
-    constructor(props){
+const HomepageConainer = (WrappedComponent) => class extends React.Component {
+    constructor(props) {
         super(props)
-        this.state ={
-            test:1
+        this.state = {
+            rooms: []
         }
     }
 
+    componentWillMount() {
+        api.room.getRooms().then((res) => {
+            const { success,items } = res.data
+            if (success) {
+                if (items && items.length > 0) {
+                    this.setState({
+                        rooms:items
+                    })
+                }
+            }
+        })
+    }
 
-    render(){
-       return(
-            <WrappedComponent {...this.state}/>
-        )  
-    }       
+
+    render() {
+        return (
+            <WrappedComponent {...this.state} {...this.props} />
+        )
+    }
 }
 
 export default HomepageConainer;
