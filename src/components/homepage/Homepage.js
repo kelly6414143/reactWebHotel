dfb81491f79860a4e9be46e9f0f67e0994aecfcb
kelly-chang <kelly6414143@gmail.com>
2020-07-22 17:57:09 +0800

    修正 homepage 樣式

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -5,35 +5,41 @@ import '../../style/homepage.scss'
 
 const Homepage = (props) => {
     const { rooms } = props
-    console.log(rooms)
     return (
         <div className="homepage">
-            <h1>WHITE INN</h1>
-            <div className="homepage_footer">
-                <div className="homepage_roomcarousel">
-                    {
-                        rooms.map((el) => {
-                            const trimRoomName = el.name.split(" ").join("")
-                            return (<Link to={`/roomInfo/${trimRoomName}`} key={el.id} className="homepage_roomcarousel_roomcard">
-                                <div className="homepage_roomcarousel_imgwrapper" style={{ backgroundImage: `url(${el.imageUrl})` }}></div>
-                                <div className="homepage_roomcarousel_roominfo">
-                                    <h3>{el.name}</h3>
-                                    <p>$ {el.normalDayPrice}</p>
-                                    <p>$ {el.holidayPrice}</p>
-                                </div>
-                            </Link>
+            <div className="homepage_wrapper">
+                <h1>WHITE INN</h1>
+                <div className="homepage_footer">
+                    <div className="homepage_roomcarousel">
+                        {
+                            rooms.map((el) => {
+                                    const trimRoomName = el.name.split(' ').join('')
+                                    return (
+                                        <Link to={ `/roomInfo/${ trimRoomName }` } key={ el.id } className="homepage_roomcarousel_roomcard">
+                                            <div className="homepage_roomcarousel_imgwrapper" style={ { backgroundImage: `url(${ el.imageUrl })` } }></div>
+                                            <div className="homepage_roomcarousel_roominfo">
+                                                <h3>{ el.name }</h3>
+                                                <p>$ { el.normalDayPrice }</p>
+                                                <p>$ { el.holidayPrice }</p>
+                                            </div>
+                                        </Link>
+                                    )
+                                }
                             )
                         }
-                        )
-                    }
-                </div>
-                <div className="homepage_webinfo">
-                    <p>台北市ＯＯ區ＯＯ街123號</p>
-                    <p>12345-6789</p>
-                    <p>abcd@efghijk.com</p>
+                    </div>
+                    <div className="homepage_webinfo">
+                        <div className="homepage_webinfo_wrapper">
+                            <p>台北市ＯＯ區ＯＯ街123號</p>
+                            <p>12345-6789</p>
+                            <p>abcd@efghijk.com</p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
+            </div>
+
     )
 }
 
