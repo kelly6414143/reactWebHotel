1e4df69cece888c7dbe6879661b3f1080bbe338b
kelly-chang <kelly6414143@gmail.com>
2020-07-22 17:57:56 +0800

    建構roompage架構

--- src/components/roompage/Roompage.js
+++ src/components/roompage/Roompage.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import RoompageConainer from '../../containers/roompage/Roompage'
+import '../../style/roompage.scss'
 
 const Roompage = (props) => {
+    console.log(props)
     return (
-        <div>Roompage</div>
+        <div className="roompage">
+            <div className="roompage_wrapper">
+                <h2>WHITE INN</h2>
+            </div>
+            <div className="roompage_info">
+                <div className="roompage_info_left"></div>
+                <div className="roompage_info_right"></div>
+            </div>
+        </div>
     )
 
 }
