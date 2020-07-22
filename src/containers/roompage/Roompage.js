fce3ecd205f710daec3b66367c1dd9e0fc74040b
kelly-chang <kelly6414143@gmail.com>
2020-07-22 17:58:32 +0800

    串接getSingleRoom api 並傳入component

--- src/containers/roompage/Roompage.js
+++ src/containers/roompage/Roompage.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React from 'react'
+import api from '../../apis/api'
 
-const RoompageConainer= (WrappedComponent)=> class extends React.Component {
-    constructor(props){
+const RoompageConainer = (WrappedComponent) => class extends React.Component {
+    constructor (props) {
         super(props)
+        this.state = {
+            room: [],
+            booking: []
+        }
     }
 
+    componentDidMount () {
+        api.room.getSingleRoom(
+            { id: '3Elqe8kfMxdZv5xFLV4OUeN6jhmxIvQSTyj4eTgIowfIRvF4rerA2Nuegzc2Rgwu' })
+            .then((res) => {
+                const { success, room, booking } = res.data
+                if (success) {
+                    this.setState({
+                        room: room,
+                        booking: booking
+                    })
+                }
+            })
+    }
 
-    render(){
-       return(
-            <WrappedComponent {...this.state}/>
-        )  
-    }       
+    render () {
+        return (
+            <WrappedComponent { ...this.state } { ...this.props } />
+        )
+    }
 }
 
-export default RoompageConainer;
+export default RoompageConainer
