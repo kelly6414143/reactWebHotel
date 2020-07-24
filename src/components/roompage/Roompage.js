2a28ddf82ff57d1edc5c4c76d0298b73d7414e33
kelly-chang <kelly6414143@gmail.com>
2020-07-24 18:07:18 +0800

    使用useEffect帶入img

--- src/components/roompage/Roompage.js
+++ src/components/roompage/Roompage.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import RoompageConainer from '../../containers/roompage/Roompage'
 
 const Roompage = (props) => {
-    console.log(props)
+    // const [room, setRoom] = useState([])
+    const[imageOne, setImageOne] = useState("")
+
+    useEffect(()=>{
+        console.log(props)
+
+        if(props.room.length>0){
+            setImageOne(props.room[0].imageUrl[0])
+        }
+        // setRoom(props.room)
+    },[])
+
+    // useEffect(()=>{
+    //     console.log(props)
+       // if(room.length>0){
+       //     setImageOne(room[0].imageUrl[0])
+       // }
+    // })
+
+
     return (
         <div className="roompage">
             <div className="roompage_wrapper">
                 <h2>WHITE INN</h2>
             </div>
             <div className="roompage_info">
-                <div className="roompage_info_left"></div>
+                <div className="roompage_info_left">
+                    <div style={ { backgroundImage: `url(${ imageOne })`} }></div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
                 <div className="roompage_info_right"></div>
             </div>
         </div>
