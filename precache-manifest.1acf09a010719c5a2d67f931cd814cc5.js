1133f68e9f2df7fbea5abdf3ca71cad7d26af1ab
kelly6414143 <kelly6414143@gmail.com>
2020-07-27 20:37:40 +0800

    Updates

---
+++ precache-manifest.1acf09a010719c5a2d67f931cd814cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c02bfac0153339539845f29d968f757",
+    "url": "/reactWebHotel/index.html"
+  },
+  {
+    "revision": "a11d278ce59ecdc6ba2f",
+    "url": "/reactWebHotel/static/css/main.3c71c194.chunk.css"
+  },
+  {
+    "revision": "47bfbe788f33782a672d",
+    "url": "/reactWebHotel/static/js/2.031a908c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactWebHotel/static/js/2.031a908c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11d278ce59ecdc6ba2f",
+    "url": "/reactWebHotel/static/js/main.594864f7.chunk.js"
+  },
+  {
+    "revision": "e0e72a3421e020104b48",
+    "url": "/reactWebHotel/static/js/runtime-main.959bc3f0.js"
+  }
+]);
