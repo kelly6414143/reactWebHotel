d8f2940e321aceecde809d288a6a2f8e686612d0
kelly-chang <kelly6414143@gmail.com>
2020-08-01 13:04:48 +0800

    帶入routeConfig, 渲染route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-// import routeConfig from './routeConfig'
+import routeConfig from './routeConfig'
 import './style/app.scss';
 import Homepage from './components/homepage/Homepage';
 import Roompage from './components/roompage/Roompage';
@@ -9,8 +9,14 @@ const App = (props) => {
   return (
     <Router >
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/roomInfo/:type" component={Roompage} />
+          {
+              routeConfig.map((route,index)=>{
+                  return(
+                      // <Route exact path={route.path} render={(props)=><route.component {...props}/>} />
+                      <Route exact path={route.path} component={route.component} />
+                  )
+              })
+          }
         <Redirect from="/roomInfo/" to="/roomInfo/Single_Room" />
         <Redirect from="*" to="/" />
       </Switch>
